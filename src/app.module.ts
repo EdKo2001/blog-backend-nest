0fc56d6ab49fa215b6c5748af761a4fd27311a6f
Eduard Koshkelyan <eduard@ez-diigtal.com>
2023-08-29 00:22:01 +0400

    feat(postgresql-connection): add

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AppController } from './app.controller';
-
 import { AppService } from './app.service';
 
-import configuration from './config/configuration';
+import configuration from '../config/configuration';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
+      envFilePath: `.env.${process.env.NODE_ENV}`,
     }),
     TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       useFactory: (config: ConfigService) => config.get('database'),
       inject: [ConfigService],
     }),
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  constructor(private readonly configService: ConfigService) {}
+
   getHello(): string {
-    return 'Hello World!';
+    const port = this.configService.get('database');
+    return `Hello! The port is: ${port.host}`;
   }
 }
